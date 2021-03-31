6d9d7b99c358e734eb7b4a7e13e0e093fec0229d
LajosTueur <geonczeol.boti@gmail.com>
2021-03-31 21:44:28 +0200

    Localization generator

---
+++ scripts/copy_localization.py
@@ -0,0 +1,19 @@
+﻿import regex
+import os
+import apply_script as pdx_parser
+import codecs
+base_folders = ("../Random New World", "../Random New World Holy Sites")
+languages = ("french", "german", "spanish", "simp_chinese", "russian", "korean")
+
+for folder in base_folders:
+    path = os.path.join(folder, "localization/english")
+    for file in os.listdir(path):
+        english = open(os.path.join(path, file)).read()
+        for lang in languages:
+            new_lang = english.replace("l_english", f"l_{lang}")
+            new_file = file.replace("l_english", f"l_{lang}")
+            new_path = os.path.join(folder, f"localization/{lang}")
+            output_file = open(os.path.join(new_path, new_file), "w")
+            output_file.write(new_lang)
+            output_file.close()
+
---
+++ scripts/shared_functions.py
@@ -0,0 +1,35 @@
+﻿import regex
+import os
+import apply_script as pdx_parser
+import codecs
+
+comment_pattern = regex.compile(r"(#)[^\n]*")
+
+def write_output(path, file, text):
+    if type(text) == list:
+        parsed_file = text
+    elif file.endswith("txt"):
+        temporary_output_file = open(os.path.join(path, file), "w")
+        temporary_output_file.write(text)
+        temporary_output_file.close()
+        parsed_file = pdx_parser.parse_file(os.path.join(path, file))
+    
+    if file.endswith("txt"):
+        byte_text = bytes(pdx_parser.reconstruct(parsed_file).encode())
+    else:
+        byte_text = bytes(text.encode())
+    output_file = open(os.path.join(path, file), "wb")
+    output_file.write(codecs.BOM_UTF8)
+    output_file.write(byte_text)
+    output_file.close()
+
+def dumpfiles(path, ending = ".txt"):
+    full_text = ""
+    for file in os.listdir(path):
+        if file.endswith(ending):
+            full_text += read_with_removed_comments(path, file)
+    return(full_text)
+
+def read_with_removed_comments(path, file):
+    file_content = open(os.path.join(path, file)).read()
+    return(comment_pattern.sub("", file_content))
